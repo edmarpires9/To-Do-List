f2e21ec3b5e71f64a8445162b546a0a60537abaa
edmar.pires9 <edmarpires18@gmail.com>
2025-09-05 15:56:36 -0300

    Feat/ Feito a UX do aplicativo.

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <h4>To-Do List</h4>
+      </div>
+      <div>
+        <form className="add-task" action="" method="post">
+          <input className="input" type="text" placeholder="Type something" />
+          <button className="button button-blue" type="submit">
+            +
+          </button>
+        </form>
+      </div>
+      <div className="task">
+        <p className="task-name">I will wake up ate 8 in the morning</p>
+        <div className="row">
+          <button className="button button-small-blue" type="submit">
+            #
+          </button>
+          <button className="button button-small-red" type="submit">
+            -
+          </button>
+        </div>
+      </div>
+      <div className="task">
+        <p className="task-name">i will pratice html for 1 hour</p>
+        <div className="row">
+          <button className="button button-small-blue" type="submit">
+            #
+          </button>
+          <button className="button button-small-red" type="submit">
+            -
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
